47dfbad84db13887c78d065976e1b57d9701c98d
Yosef-Perelman <yosefper@gmail.com>
2025-01-14 18:53:34 +0200

    Added test for ActionsOfTheParkingLot.

--- src/main/java/models/ActionsOfTheParkingLot.java
+++ src/main/java/models/ActionsOfTheParkingLot.java
@@ -8,34 +8,34 @@ import java.time.LocalTime;
 public class ActionsOfTheParkingLot {
 
     public void enter(Vehicle vehicle, int id, ParkingSpace someAvailableSpace){
-        updateEnteringInVehicleAndParkingSpace(vehicle, id, someAvailableSpace);
-        updateListsOfEntering(vehicle, id, someAvailableSpace);
+        updatingVehicleAndParkingSpaceAboutEntering(vehicle, id, someAvailableSpace);
+        updateListsAboutEntering(vehicle, id, someAvailableSpace);
     }
 
-    private static void updateEnteringInVehicleAndParkingSpace(Vehicle vehicle, int id, ParkingSpace someAvailableSpace){
+    private static void updatingVehicleAndParkingSpaceAboutEntering(Vehicle vehicle, int id, ParkingSpace someAvailableSpace){
         someAvailableSpace.setTaken(true);
         someAvailableSpace.setCarInSpace(vehicle.getNumber());
         vehicle.setEnterTime(LocalTime.now());
         vehicle.setParkingSpaceId(id);
     }
 
-    private static void updateListsOfEntering(Vehicle vehicle, int id, ParkingSpace someAvailableSpace){
+    private static void updateListsAboutEntering(Vehicle vehicle, int id, ParkingSpace someAvailableSpace){
         ParkingLot.getInstance().getAvailableParkingSpaces().get(vehicle.getType()).remove(id);
         ParkingLot.getInstance().getOccupiedParkingSpaces().get(vehicle.getType()).put(vehicle.getNumber(), someAvailableSpace);
     }
 
     public void leave(Vehicle vehicle, ParkingSpace someAvailableSpace){
-        updateLeavingInVehicleAndParkingSpace(vehicle, someAvailableSpace);
-        updateListsOfLeaving(vehicle, someAvailableSpace);
+        updatingVehicleAndParkingSpaceAboutLeaving(vehicle, someAvailableSpace);
+        updateListsAboutLeaving(vehicle, someAvailableSpace);
     }
 
-    private void updateLeavingInVehicleAndParkingSpace(Vehicle vehicle, ParkingSpace spaceOfTheCar){
+    private void updatingVehicleAndParkingSpaceAboutLeaving(Vehicle vehicle, ParkingSpace spaceOfTheCar){
         spaceOfTheCar.setCarInSpace(null);
         spaceOfTheCar.setTaken(false);
         vehicle.setParkingSpaceId(0);
     }
 
-    private void updateListsOfLeaving(Vehicle vehicle, ParkingSpace spaceOfTheCar){
+    private void updateListsAboutLeaving(Vehicle vehicle, ParkingSpace spaceOfTheCar){
         ParkingLot.getInstance().getOccupiedParkingSpaces().get(vehicle.getType()).remove(vehicle.getNumber());
         ParkingLot.getInstance().getAvailableParkingSpaces().get(vehicle.getType()).put(spaceOfTheCar.getId(), spaceOfTheCar);
     }
